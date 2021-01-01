da39ce74e2eaac2cf2f13c49723c8f57707482bd
Joshua Hughes <plutonus.stream@gmail.com>
2021-01-01 23:57:27 +1000

    Revert "Feature 0.0.1"

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const logger = require("./logger");
-const master = require("./master");
-const api = require("./api");
-
-module.exports = (database) => ({
-	logger,
-	master,
-	api: api(database),
-});
