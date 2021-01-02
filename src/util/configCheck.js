a4c5a538abc80a47b3def2b58643d827e5b8aa09
PlutonusDev <plutonus.stream@gmail.com>
2021-01-02 10:52:00 +0000

    Added more configCheck rules.

--- src/util/configCheck.js
+++ src/util/configCheck.js
@@ -8,6 +8,9 @@ module.exports = new Promise((res) => {
 	if (!config.SESSION_SECRET) error("'SESSION_SECRET' is not set. This may be security issue. Defaulted to: 'changeme'.");
 	if (config.SESSION_SECRET && config.SESSION_SECRET.length < 8) error("'SESSION_SECRET' is shorter than 8 characters. This may be a security issue.");
 	if (!config.PORT) warn("'PORT' is not set. Defaulted to: '3000'.");
+	if (!config.DISCORD_ID) error("'DISCORD_ID' is not set. Users will not be able to login with Discord.");
+	if (!config.DISCORD_SECRET) error("'DISCORD_SECRET' is not set. Users will not be able to login with Discord.");
+	if (!config.DISCORD_REDIRECT) error("'DISCORD_REDIRECT' is not set. Users will not be able to login with Discord.");
 	if (!config.DB_URL) warn("'DB_URL' is not set. Defaulted to: 'mongodb://localhost/filehost'.");
 	log("Configuration check completed.\n");
 	res();
